74bbdae616c3e7c1fdbe49fc63e72ecf5636b66e
vrralf <vrralf@gmail.com>
2022-11-11 01:40:56 -0300

    first commit

---
+++ src/test/java/com/example/dockerExample/DockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
